f72f6ce63cdad570a0a680af75d8a4f2f6a4fe8d
Hubert Moncenis <hubmoncenis@gmail.com>
2018-03-17 13:28:26 +0100

    Create a model function to save a tweet in mysql db

--- app/Console/Commands/GetTweetsFDC.php
+++ app/Console/Commands/GetTweetsFDC.php
@@ -28,14 +28,13 @@ class GetTweetsFDC extends Command
 
     /**
      * Create a new command instance.
-     * @param string $drip
      * @return void
      */
     public function __construct()
     {
-        parent::__construct();
-        $query = 'FilsDeCulte';
-        $this->query = $query;
+      parent::__construct();
+      $query = 'FilsDeCulte';
+      $this->query = $query;
     }
 
     /**
@@ -48,13 +47,13 @@ class GetTweetsFDC extends Command
         /* Search on twitter all the tweets in relation w/ our bot
         and get the number of them */
         $parameters = [
-            'q' => $this->query
+          'q' => $this->query
         ];        
         $allTweets = Twitter::getSearch($parameters);
 
-        /* For reach tweet, we check if the model Tweet validate our tweet, 
-        and add it to the selectedTweets array */
         foreach ($allTweets->statuses as $key => $tweet) {
+          /* For each tweet, we check if the model Tweet validate our tweet, 
+          and add it to the selectedTweets array */          
           if(Tweet::isEligible($tweet)) {
             $selectedTweets[$key]['id_tweet'] = $tweet->id_str;
             $selectedTweets[$key]['user'] = $tweet->user->screen_name;
@@ -67,41 +66,38 @@ class GetTweetsFDC extends Command
             /* We get the list of users mentionned and verify if there are two.
             For each of them, we check if it's not our bot. 
             We define then the target and add it to the selectedtweets array.
+            If there are not two user mentionned, we stop the current tweet verification and go to
+            the next.
             */
             $userMentions = $tweet->entities->user_mentions;
 
             foreach ($userMentions as $id => $value) {
-              if($value->name != BOT_ACCOUNT && count($userMentions === 2)) {
+              if($value->name != BOT_ACCOUNT) {
                 $target_id = $tweet->entities->user_mentions[$key]->id_str;
                 $target = $tweet->entities->user_mentions[$id]->screen_name;
                 $selectedTweets[$key]['target'] = [
-                  'status' => 1,
                   'id' => $target_id,
                   'profil' =>$target,
                 ];
                 break;
               }
-            }
+            }  
 
+            Tweet::insertTweetInDatabase($selectedTweets[$key]);
+           // dd($selectedTweets[$key]);
+
+
+
+            /* If the validation is'nt good, we keep the unconformed tweet to an array,
+            and we will push a private message to the author to explain him that his tweet isn't good.
+            and give maybe a link to him which explain how to tweet with our bot.
+            */
           } else {
             $notSelectedTweets[] = $tweet;
           }
-
-          dd($selectedTweets);
         }
 
 
 
-
-
-
-
-        // $tweets = Tweet::isEligible($allTweets);
-
-        // print_r($tweets);
-
-        // model isEll::dsdlmksl(allTweets)
-        //Init an empty array of our selected Tweets
-        //$selectedTweets = [];
+      }
     }
-}
--- app/Tweet.php
+++ app/Tweet.php
@@ -20,8 +20,6 @@ class Tweet extends Model
     protected $table = 'tweets';
 
 
-    // var_dump($table)
-
     public static function isEligible($singleTweet) {
 
    		// get the time of each tweet and convert it to Carbon date format
@@ -36,112 +34,50 @@ class Tweet extends Model
     	// Check the source of the tweet
     	$source = $singleTweet->user->screen_name;
 
+    	// We verify that the tweet contain a hashtag 
     	$hashtags = count($singleTweet->entities->hashtags);
 
+
+    	// We verify that the tweet mentionned exactly two people
+    	$userMentions = count($singleTweet->entities->user_mentions);
+
+    	// We check in the database if the tweet already exist
 	    $existingTweet = self::where('id_tweet', $singleTweet->id_str)->exists();	
 
+
+	    /*
+				If the tweet is insside the defined time && it doesn't come from
+				our bot && it has exactly one hashtag (one movie) && it doesn't exist 
+				in our db, we return true, which means that the tweet is elligible
+	    */
       if($limit_time <= LIMIT_TIME && 
       		$source != BOT_ACCOUNT && 
       		$hashtags === 1 && 
+      		$userMentions === 2 &&
       		!$existingTweet
       	) {
     				return true;
       } 
 
-       //  if (!empty($existingTweet)) {
-       //  	$error = ['duplicate', $existingTweet];
-       //  	$this->sendResponseTweets($error);
-       //  	continue;
-      	// }
-	   	// 	var_dump($tweet);
-
-    	// }
-
-    	// print_r($selectedTweets);
-
     }
 
-   //  function isEligible($allTweets){
+    public static function insertTweetInDatabase($singleTweet) {
+
+    	// dd($singleTweet);
+
+    	$tweet = new Tweet;
+
+    	$tweet->id_tweet = $singleTweet['id_tweet'];
+    	$tweet->user_tweet =  $singleTweet['user'];
+    	$tweet->tweet_user_id = $singleTweet['user_id'];
+    	$tweet->target_tweet =  $singleTweet['target']['profil'];
+    	$tweet->target_user_id = $singleTweet['target']['id'];
+    	$tweet->movie_title = $singleTweet['movie'];
+    	$tweet->created_tweet_time = $singleTweet['created_at'];
+    	$tweet->spoil = '';
+    	$tweet->isSpoiled = $singleTweet['isSpoiled'];
 
 
-
-	  //     // get the time of each tweet and convert it to Carbon date format
-	  //     $initial_date = $allTweets->statuses[$tweet]->created_at;
-	  //     $new_date = Carbon::parse($initial_date);
-
-	  //     // Define the limite time and check if each tweet is inside it or no
-	  //     $limit_time = $new_date->diffInMinutes(Carbon::now());
-
-	  //     // Check the source of the tweet
-	  //     $source = $allTweets->statuses[$tweet]->user->screen_name;
-
-	  //     $existingTweet = DB::table('tweet')->where('id_tweet', $allTweets->statuses[$tweet]->id_str)->get()->toArray();
-
-
-	  //     if (!empty($existingTweet)) {
-	  //       $error = ['duplicate', $existingTweet];
-	  //       $this->sendResponseTweets($error);
-	  //       continue;
-	  //     }
-
-	  //     if($limit_time <= 60 && $source != BOT_ACCOUNT && empty($existingTweet)) {
-
-	  //       // get the tweet reference id
-	  // 		  $selectedTweets[$tweet]['id_tweet'] = $allTweets->statuses[$tweet]->id_str;
-
-	  // 		  // get the author of the tweet
-	  // 		  $selectedTweets[$tweet]['user'] = $allTweets->statuses[$tweet]->user->screen_name;
-
-	  //       // get the id of the tweet author
-	  //       $selectedTweets[$tweet]['user_id'] = $allTweets->statuses[$tweet]->user->id_str;
-
-	  // 		  // get the profil pic of the author
-	  // 		  $selectedTweets[$tweet]['user_profile'] = $allTweets->statuses[$tweet]->user->profile_image_url_https;
-	            
-	  //       // get the time where the tweet has been created
-	  //       $selectedTweets[$tweet]['created_at'] = $new_date->toTimeString();
-
-	  //       // set the boolean isSpoiled to false
-	  //       $selectedTweets[$tweet]['isSpoiled'] = 0;
-
-
-	  //       // Verify we have only one movie as hashtag and get it
-	  //       if(count($allTweets->statuses[$tweet]->entities->hashtags) === 1) {
-	  //        $selectedTweets[$tweet]['movie'] = $allTweets->statuses[$tweet]->entities->hashtags[0]->text;
-	  //       } 
-
-	  //   		// Check all users mentionned and eject FilsDeCulte
-	  //   		foreach ($allTweets->statuses[$tweet]->entities->user_mentions as $key => $value) {
-
-	  //   			if($value->name != BOT_ACCOUNT) {
-	  //   				$target = $allTweets->statuses[$tweet]->entities->user_mentions[$key]->screen_name;
-	  //           $target_id = $allTweets->statuses[$tweet]->entities->user_mentions[$key]->id_str;
-
-	  //   				$selectedTweets[$tweet]['target'] = [
-	  //             'status' => 1,
-	  //             'id' => $target_id,
-	  //             'profil' =>$target,
-	  //           ];
-	  //         // or set the target to 0
-	  //   			} else {
-	  //           $selectedTweets[$tweet]['target'] = 0;
-	  //         }
-	  //   		}
-
-	  //     }   
-	  //  	}
-
-	  //   // if our new tweet array has one or more tweet
-	  //   if(count($selectedTweets) > 0) {
-
-	  //     /* we call the insert to db method, which will put the tweet content to 
-	  //        the database */
-	  //      // $this->clearDatabase();
-	  //      $this->insertToDatabase($selectedTweets);
-	  //      $this->getSpoil($selectedTweets);
-	  //   }
-
-	  //  	return view('pages.new-tweets', ['tweets' => $selectedTweets]);
-	  // }
-
+    	$tweet->save();
+    }
 }
