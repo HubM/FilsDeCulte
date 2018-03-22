4e90dba576125bf60c9506c5f7b3b9a809f80e8a
Hubert Moncenis <hubmoncenis@gmail.com>
2018-03-22 11:55:12 +0100

    Create a controller which will make all the logic necessary to get informations for statistics

---
+++ app/Http/Controllers/StatsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Tweet;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+class StatsController extends Controller
+{
+  public function getStats() {
+
+  	$tweets = Tweet::get();
+
+	  /*
+			Variables pour gestion des tweets échouées
+			--------
+
+  		$failedTweets = Tweet::where('isFailed', 1);
+  		$get_failedTweets = $failedTweets->get();
+  		$count_failedTweets = $failedTweets->count();
+		*/
+
+  	/*
+			Variables pour connaitre le meilleur spoiler
+			---------
+	  	$query_best_spoiler = Tweet::select(DB::raw("user_tweet, count(user_tweet) as best_spoiler"))
+	  												 							->groupBy('user_tweet')
+	  												 							->orderBy("best_spoiler",'desc')
+	  												 							->limit(2)
+	  												 							->get();
+
+  		$best_spoiler = $query_best_spoiler[0]->user_tweet;
+  	*/
+
+  	/*
+			Variables pour obtenir les tweets du jours et leur nombre
+			---------
+	  	foreach ($tweets as $tweet => $value) {
+	  		$tweet_date = Carbon::parse($value->created_at);
+
+	  		if($tweet_date->isToday()) 
+	  			$today_tweets[$tweet] = $value;
+	  	}
+
+	  	$today_tweets - Les tweets du jours;
+	  	$nb_today_tweets = count($today_tweets); - Le nombre de tweet du jour
+  	*/
+
+	  /*
+			Variables pour obtenir le films le plus tweeté
+			---------
+
+			$query_best_movies = Tweet::select(DB::raw("movie_title, count(movie_title) as best_movies"))
+  												 							->groupBy('movie_title')
+  												 							->orderBy("best_movies",'desc')
+  												 							->limit(1)
+  												 							->get();
+
+			$best_movie = $query_best_movies[0]->movie_title;
+	  */
+
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function(){
-	return view('welcome');
-});
+// Route::get('/', function(){
+// 	return view('welcome');
+// });
+
+Route::get('/', 'StatsController@getStats');
 
 Route::get('/new-tweets', 'TwitterBotController@getNewTweets');
 
