121d01f70c89b5ae48aa99f3b4d0dcb8317f8e8a
Hubert Moncenis <hubmoncenis@gmail.com>
2018-05-31 15:53:17 +0200

    Layout for stats page

--- resources/views/pages/statistics.blade.php
+++ resources/views/pages/statistics.blade.php
@@ -1,52 +1,40 @@
 @extends('layout.base')
-@section('body-class')stats colored @endsection
+@section('body-class')stats @endsection
 @section('content')
-	<div class="container">
-		<section class="stats">
-			<header>
-				<a href="./">
-					<img src="./images/logo_fdc.svg" alt="Logo fils de culte" class="logo">
-				</a>
-				<h1>Stats</h1>
-				<p>In this page, we will present you different statistics we got =)</p>
-			</header>
-			<main>
-				<ul class="infos_cards">
-					@if ($datas)
-						@if ($datas['all_tweets'] > 0)
-							<li class="card">
-								<div class="card-body text-center">
-									<h2 class="card-title">
-										{{ $datas['all_tweets'] }}
-									</h2>							
-									<h3 class="card-subtitle mb-2 text-muted">Tweets made since the beginning :D</h3>
-									<p class="card-text">Among them, <span>{{ $datas['counter_tweets_spoiled'] }}</span> have spoiled the target and <span>{{ $datas['counter_tweets_failed'] }}</span> have failed !</p>
-								</div>
-							</li>
-						@endif
-						@if (!empty($datas['best_spoiler']))
-							<li  class="card">
-								<div class="card-body text-center">
-									<h2 class="card-title">
-										{{ $datas['best_spoiler'] }}
-									</h2>	
-									<h3 class="card-title mb-2 text-muted">is the best spoiler, congratulations ! :D</h3>
-								</div>						
-							</li>
-						@endif
-						@if (!empty($datas['best_movie']))
-							<li  class="card">
-								<div class="card-body text-center">
-									<h2 class="card-title">
-										{{ $datas['best_movie'] }}
-									</h2>	
-									<h3 class="card-title mb-2 text-muted">is the best spoiled movie, go watch it ! :D</h3>
-								</div>						
-							</li>					
-						@endif 
+	<section class="container">
+		<header>
+			<a href="./">
+				<img src="./images/logo_fdc.svg" alt="Logo fils de culte" class="logo">
+			</a>	
+			<h1>Stats</h1>
+			<p>In this page, we will present you different statistics we got =)</p>
+		</header>
+		<main>
+			@if ($datas)
+				<ul class="row stats-list">
+					@if ($datas['all_tweets'] > 0)
+						<li class="col-md-6 col-lg-4 tweets">
+							<img src="./images/tweets.png" alt="Tweets flat icon">
+							<h2>{{ $datas['all_tweets'] }}</h2>							
+							<p>Tweets made since the beginning :D ! Among them,  <span>{{ $datas['counter_tweets_spoiled'] }}</span> have <span>spoiled</span> their target and <span>{{ $datas['counter_tweets_failed'] }}</span> have <span>failed</span> !<p>
+						</li>
+					@endif
+					@if (!empty($datas['best_spoiler']))
+						<li class="col-md-6 col-lg-4 best-spoiler">
+							<img src="./images/trophy.png" alt="Trophy flat icon">
+							<h2>{{ $datas['best_spoiler'] }}</h2>	
+							<p>is the best spoiler, congratulations ! :D</p>
+						</li>
+					@endif
+					@if (!empty($datas['best_movie']))
+						<li class="col-md-6 col-lg-4">
+							<img src="./images/movie.png" alt="Movie flat icon">
+							<h2>{{ $datas['best_movie'] }}</h2>	
+							<p>is the best spoiled movie, go watch it ! :D</p>
+						</li>					
 					@endif 
 				</ul>
-			</main>
-		</section>
-	</div>
+			@endif 
+		</main>
+	</section>
 @endsection
